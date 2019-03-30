a92188a05563df27eba09761d6f6e50e8a3a4914
Tom Hodgson <tom.hodgson@salecycle.com>
2019-03-30 10:53:52 +0000

    add a test to append the letter a to a string

---
+++ src/main/java/com/sample/Unit_gcytgt_root.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+import java.lang.String;
+
+public class Unit_gcytgt_root {
+  private final Unit_gcytgt_root_0 unit_gcytgt_root_0;
+
+  public Unit_gcytgt_root(final Unit_gcytgt_root_0 unit_gcytgt_root_0) {
+    this.unit_gcytgt_root_0 = unit_gcytgt_root_0;
+  }
+
+  public String execute(final String contents) {
+    final String string = unit_gcytgt_root_0.execute(contents);
+    return string;
+  }
+}
